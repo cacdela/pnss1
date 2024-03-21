c42ac22a02c374e6c61b7e32ced0e8ca30e5e230
cacdela <vladimircacdela@gmail.com>
2024-03-21 23:53:43 +0700

    работа с композером

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -9,7 +9,15 @@ require_once __DIR__ . '/../vendor/autoload.php';
 function getConfigs(string $path = DIR_CONFIG): array
 {
    $settings = [];
-	@@ -26,4 +21,3 @@ function getConfigs(string $path = DIR_CONFIG): array
+   foreach (scandir(__DIR__ . $path) as $file) {
+       $name = explode('.', $file)[0];
+       if (!empty($name)) {
+           $settings[$name] = include __DIR__ . "$path/$file";
+       }
+   }
+   return $settings;
+}
+
 require_once __DIR__ . '/../routes/web.php';
 
 return new Src\Application(new Src\Settings(getConfigs()));
